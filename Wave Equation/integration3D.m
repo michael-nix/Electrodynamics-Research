970500a67813cab78301e91b8fc2686063beddd3
michael-nix <61841150+michael-nix@users.noreply.github.com>
2020-06-25 15:08:21 -0400

    Update integration3D.m

--- Wave Equation/integration3D.m
+++ Wave Equation/integration3D.m
@@ -1,7 +1,16 @@
 % setup workspace:
-tmax = 150; n = 128; x = linspace(-20, 20, n);
+n = 128;   rmin = -20;   rmax = 20;
+
+x = linspace(rmin, rmax, n);
 [x, y, z] = meshgrid(x, x, x);
-c = 10; dt = 0.01; dr = y(2) - y(1); s = dt^2 * c^2;
+
+c = 10;   dt = 0.01;   dr = y(2) - y(1);   s = dt^2 * c^2;
+
+% ensure stability via CFL condition:
+while s / dr^2 >= 1
+    dt = dt * 0.95;
+    s = dt^2 * c^2;
+end
 
 % initialize scalar potential (u) and source (f):
 f = exp((-x.^2 - y.^2 - z.^2) / 2) / sqrt((2 * pi)^3);
@@ -13,10 +22,14 @@ m = n / 2;
 x1D = x(m, :, m);
 psi1D = zeros(n, 5);
 
-fig1 = figure; hold on;
+% clear up some memory and get ready to loop:
+clear x y z;
 
-index = 1;
 plot_times = [5, 10, 50, 100, 150];
+tmax = max(plot_times);
+idx = 1;
+
+fig1 = figure;   hold on;
 
 % loop through explicit method over time of interest:
 for t = 1:tmax
@@ -25,18 +38,23 @@ for t = 1:tmax
     u_now = u_next;
     
     if any(t == plot_times)
-        psi1D(:, index) = u_now(m, :, m);
+        psi1D(:, idx) = u_now(m, :, m);
         
         figure(fig1);
-        plot(x1D, psi1D(:, index), 'LineWidth', 2);
+        plot(x1D, psi1D(:, idx), 'LineWidth', 2);
         drawnow;
         
-        index = index + 1;
+        idx = idx + 1;
     end
 end
 
-plot(x1D, 1/4/pi./abs(x1D), 'k--');
-axis([-20 20 0 0.07]);
-xlabel('x (au)');
-ylabel('Scalar Potential, \phi \rightarrow 1 / 4\pir');
-legend('t = 0.05','t = 0.1','t = 0.5','t = 1.0','t = 1.5','1/(4\pir)');
+ncells = length(plot_times) + 1;
+labels = cell(ncells, 1);
+for i = 1:(ncells - 1)
+    labels{i} = ['t = ', num2str(dt * plot_times(i))];
+end
+labels{end} = '1/(4\pir)';
+
+plot(x1D, 1/4/pi./abs(x1D), 'k--');   axis([-20 20 0 0.07]);
+xlabel('x (au)');   ylabel('Scalar Potential, \phi \rightarrow 1 / 4\pir');
+legend(labels);
