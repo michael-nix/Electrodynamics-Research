f4b48acebe862469373ef77b3271e47529d0209d
michael-nix <61841150+michael-nix@users.noreply.github.com>
2020-08-04 21:51:28 -0400

    Update testQMSolver2D.m

--- Non-Relativistic QM/testQMSolver2D.m
+++ Non-Relativistic QM/testQMSolver2D.m
@@ -1,4 +1,4 @@
-clear all; import CommonFunctions.* Fields.* Potentials.*; %#ok<CLALL>
+clear all;   import CommonFunctions.* Fields.* Potentials.*; %#ok<CLALL>
 % TESTQMSOLVER2D  Script to test QuantumSolver2D and run some experiments.
 %   
 %   Modify the electric potential, pot, and magnetic field, mag, to run
@@ -12,17 +12,17 @@ clear all; import CommonFunctions.* Fields.* Potentials.*; %#ok<CLALL>
 %   
 %   See also QSPARAMETERS, QUANTUMSOLVER1D, QUANTUMSOLVERS.
 
-pot = @zero_potential;
+pot = @harmonic_potential;
 
-want2plot = true;
+want2plot = false;
 
 % 2^14 @ 128 = < 40 s.
 % 2^14 @ 256 = < 270 s.
 % 2^14 @ 512 = < 1100 s.
 % 2^14 @ 1024 = < 4450 s.
-tmax = 2^14; n = 256;
+tmax = 2^14;    n = 256;
 
-rmax = 50;
+rmax = 10;
 x = linspace(-rmax, rmax, n);
 y = linspace(-rmax, rmax, n);
 dx = abs(x(2) - x(1));
@@ -33,44 +33,37 @@ dy = abs(y(2) - y(1));
 % theta = atan2(y, x);
 sig = 1;
 
-u0 = initeven(x, 0, 0, 2).*initeven(y, 0, 0, 2);
-u0 = u0 / sqrt(sum(sum(abs(u0).^2 * dx * dy)));
-
-% double slit experiment setup:
-% u0 = initeven(x, -7).*initeven(y, 0, 0, 3).*exp(1i*x*5);
-% xrange = (x > -5 & x < -3);
-% pot = 1000*(1 - ((y > 0.25 & y < 1) + (y > -1 & y < -0.25))).*xrange;
-% pml = zeros(size(x));   idx = abs(x) > (10 - 15*dx);
-% pml(idx) = log(1e4)*5/(15*dx)^5 * (abs(x(idx)) - (10 - 15*dx)).^4;
-% pml = pml + pml.';   pot = pot - 1i*pml;
-% clear idx xrange;
+u0 = initeven(x, 0, 0, 2).*initeven(y, 0, -2, 2);
+u0 = u0 / sqrt(sum(abs(u0).^2 * dx * dy, 'all'));
 
 [~, ~, dt] = getfft(u0, dx, dy);
 
-qsp = QSParameters({x, y}, dt, pot, zeros(size(u0)));   qsp.q = -1;
-% qsp = QSParameters({x, y}, dt, pot);
+% qsp = QSParameters({x, y}, dt, pot, zeros(size(u0)));   qsp.q = -100;
+qsp = QSParameters({x, y}, dt, pot);
 clear x y r theta;
 
 solver = QuantumSolver2D(qsp);
 
 ut0 = u0;
-ut1 = zeros(size(u0));
-Et = zeros(tmax,1);
+ut1 = 0.1*u0; % zeros(size(u0));
+Et = zeros(tmax, 1);
 
 % we'll sample our function at (kx, ky); this gets us a nice distribution:
 nk = 20;
 k = linspace(0, rmax, nk) / rmax * n/2;
-phi = (0:(nk-1))*(pi*(1-sqrt(5)));
+phi = (0:(nk-1)) * (pi*(1 - sqrt(5)));
 kx = round(k.*cos(phi) + n/2);
 ky = round(k.*sin(phi) + n/2);
 
-V = zeros(size(ut0));
-Ax = zeros(size(ut0));
-Ay = zeros(size(ut0));
+if ~isempty(qsp.mag)
+    V = zeros(size(ut0));
+    Ax = zeros(size(ut0));
+    Ay = zeros(size(ut0));
+end
 
 if want2plot
-    fig = mesh(qsp.x, qsp.y, abs(ut0));
-    axis([-rmax rmax -rmax rmax 0 .6]);
+    fig = mesh(qsp.x, qsp.y, abs(ut0)); %#ok
+    axis([-rmax rmax -rmax rmax 0 0.6]);
 end
 
 tic;
@@ -89,8 +82,8 @@ for t = 1:tmax
         Et(t) = Et(t) + ut0(kx(i), ky(i));
     end
     
-    if want2plot && ~mod(t-1, 1)
-        fig.ZData = abs(ut0);
+    if want2plot
+        fig.ZData = abs(ut0); %#ok
         drawnow;
     end
     
@@ -101,9 +94,7 @@ qsr = QSResults(Et, dt);
 qsr.spectrum;
 drawnow;
 
-clear solver.solve;
-clear mex; %#ok<CLMEX>
-% finished;
+clear solver.solve mex; %#ok<CLMEX>
 
 function displayProgress(t, tmax)
     progress_counter = round(0.05 * tmax);
@@ -114,9 +105,3 @@ function displayProgress(t, tmax)
         toc
     end
 end
-
-% function finished
-%     for t = 1:5
-%         beep; pause(.5);
-%     end
-% end
